ad18f249351cb113bb08cbf5820f7cf9392dc81c
DimarcusMoua <tm149864@umconnect.umt.edu>
2023-12-13 21:22:03 -0700

    js

--- assignment-14/main.js
+++ assignment-14/main.js
@@ -1,6 +1,17 @@
 var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2D");
+var ctx = c.getContext("2d");
+var cty = c.getContext("2d");
 
+ctx.beginPath();
+ctx.moveTo(200, 360);
+ctx.lineTo(300, 500);
+ctx.lineTo(100, 500);
+ctx.closePath();
+ctx.fillStyle = 'red';
+ctx.fill();
+ctx.stroke();
+
+/*
 ctx.moveTo(0,0);
 ctx.lineTo(200,100);
 ctx.stroke();
@@ -10,3 +21,4 @@ var ctx = c.getContext("2d");
 ctx.beginPath();
 ctx.arc(95, 50, 40, 0, 2 * Math.PI);
 ctx.stroke();
+*/
